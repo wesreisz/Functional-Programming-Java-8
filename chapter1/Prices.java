8540fd550343e953cec315a24a441e47ec9062c2
Wesley Reisz <wes@wesleyreisz.com>
2015-05-01 14:28:55 -0400

    updated pricing example with a declarative approach that leverages lambdas and functional operators

--- chapter1/Prices.java
+++ chapter1/Prices.java
@@ -3,13 +3,26 @@ import java.math.*;
 //Tasks:
 //Suppose we’re asked to total the prices greater than $20, discounted by 
 //10%. Let’s do that in the habitual Java way first.
+// getDiscountedTotal: does this the traditional imperative way
+// getDiscountedTotalFunctionally: does this example declaratively with lamdbas
 		
 
 public class Prices{
 	public static void main(String... args){
+		long startTime,endTime; 
 		final List<BigDecimal> prices = setPrices();
-		BigDecimal total = getDiscountedTotal(prices);
-		System.out.println("Discounted Total: " + total);
+
+		startTime = System.nanoTime();
+		BigDecimal total1 = getDiscountedTotal(prices);
+		endTime = System.nanoTime();
+		System.out.println("Discounted Total: " + total1);
+		System.out.println("Executed in: " + (endTime - startTime)/1e6);
+
+		startTime = System.nanoTime();
+		BigDecimal total2 = getDiscountedTotalFunctionally(prices);
+		endTime = System.nanoTime();
+		System.out.println("Discounted Total: " + total2);
+		System.out.println("Executed in: " + (endTime - startTime)/1e6);
 
 	}
 	private static List<BigDecimal> setPrices(){
@@ -26,5 +39,11 @@ public class Prices{
 					totalOfDiscountedPrices.add(price.multiply(BigDecimal.valueOf(0.9)));
 		}
 		return totalOfDiscountedPrices;
+	}
+	private static BigDecimal getDiscountedTotalFunctionally(List<BigDecimal>prices){
+		return prices.stream()
+				.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+				.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
 	}
 }
