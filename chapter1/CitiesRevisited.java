1774e69e1c54873ffe4a9b7149aa81d3ed385bfe
Wesley Reisz <wes@wesleyreisz.com>
2016-04-25 08:12:00 -0400

    Cities Script from the book

---
+++ chapter1/CitiesRevisited.java
@@ -0,0 +1,26 @@
+//Task Declarative Approach to find if Chicago
+//is in a given collection of cities.
+public class CitiesRevisited{
+	public static void main(String[] args) throws Exception{
+		//protect the input
+		if(args==null || args.length<=0){
+			throw new Exception("No values provided. Please pass a list of cities.");
+		}
+
+		//check the result
+		boolean result = false;
+		for(String city : args){
+			if ("Chicago".equalsIgnoreCase(city)){
+				result = true;
+				break; //no reason to iterate the list if the city is found.
+			}
+		}
+
+		//report
+		if(result){
+			System.out.println("Chicago was found");
+		}else{
+			System.out.println("Chicago was NOT found");
+		}
+	}
+}
